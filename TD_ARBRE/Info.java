9ee8af4bf5b358b1c7094ac986bd5db8dcb447c5
hamdouni <brahim@hamdouni.com>
2015-12-08 19:23:21 +0100

    Correction TD sur les arbres du 08/12/2015

---
+++ TD_ARBRE/Info.java
@@ -0,0 +1,11 @@
+public class Info {
+	public int idx, col;
+	public Info(int i, int c) {
+		idx = i;
+		col = c;
+	}
+	public String toString() {
+		return "idx = " + idx + " col = " + col;
+	}
+}
+
---
+++ TD_ARBRE/TestParcoursArbre.java
@@ -0,0 +1,54 @@
+import java.util.* ;
+
+
+/**
+ * @author Brahim HAMDOUNI (http://brahim.hamdouni.com)
+ * Parcours en profondeur d'un arbre implémenté sous forme de tableau de 4 colonnes
+ * Tableau à 2 dimensions, ligne 0 correspond à la racine
+ * col 0 : valeur stocké
+ * col 1 à 3 : index des fils (maximum 3), -1 dans la colonne si pas de fils
+ *
+ */
+public class TestParcoursArbre {
+
+	public static void main(String[] args) {
+		int[][] T = {
+			{5,  1,  2,  3},
+			{9,  4,  5, -1},
+			{8,  6, -1, -1},
+			{4, -1, -1, -1},
+			{3, -1, -1, -1},
+			{6, -1, -1, -1},
+			{7, -1, -1, -1}
+		};
+
+		Stack<Info> pile = new Stack<Info> ();
+
+		pile.push(new Info(0,1));
+
+		Info current = null;
+		int idxFils = 0;
+
+		while (! pile.empty()) {
+			current = pile.pop();
+
+			//if (current.col == 1) System.out.println(T[current.idx][0]);	// c'est la première colonne de notre noeud, 
+											// donc c'est la 1ere fois qu'on passe sur ce noeud, 
+											// on affiche sa valeur
+			System.out.println(T[current.idx][0]);				// version L. Finta
+
+			idxFils = T[current.idx][current.col]; 				// on regarde l'index du noeud fils en cours
+			if (idxFils != -1) {	
+				if(current.col < 3) {					 					
+					current.col ++;					// on prépare le prochain traitement à la colonne suivante
+					pile.push(current);				// (sauf si on est deja à la dernière)
+
+				}
+				pile.push(new Info(idxFils,1));
+			}
+		}
+
+
+	}
+	
+}
